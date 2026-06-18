9fcb88366e1c91b10c6ddf4b2c04f55352eba4f7
yasintornaci <yasintornaci20@gmail.com>
2026-06-18 15:02:04 +0300

    Add MainTab navigation definition

---
+++ app/src/main/java/com/fitnessapp/ui/navigation/MainTab.kt
@@ -0,0 +1,19 @@
+package com.fitnessapp.ui.navigation
+
+enum class MainTab(
+    val label: String,
+    val shortLabel: String
+) {
+    Statistics(
+        label = "Statistics",
+        shortLabel = "Stats"
+    ),
+    Home(
+        label = "Home",
+        shortLabel = "Home"
+    ),
+    History(
+        label = "History",
+        shortLabel = "Hist"
+    )
+}
